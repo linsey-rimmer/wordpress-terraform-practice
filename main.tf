daed0c605c6753d0bc1ec5c1cf11098adb7b889d
linsey-rimmer <linsey.rimmer@devopsgroup.com>
2022-11-22 11:21:12 +0000

    Save point

--- main.tf
+++ main.tf
@@ -261,12 +261,18 @@ data "template_cloudinit_config" "config" {
     content_type = "text/x-shellscript"
     content = <<-EOF
       #!/bin/bash
+      db_RDS=${db_RDS}
       yum -y install httpd php php-mysql
       wget https://wordpress.org/wordpress-5.1.1.tar.gz
       tar -xzf wordpress-5.1.1.tar.gz
       cp -r wordpress/* /var/www/html
       rm -rf wordpress*
       cd /var/www/html
+      sudo cp wp-config-sample.php wp-config.php
+      sed -i 's/database_name_here/mywordpressdb/' wp-config.php
+      sed -i 's/username_here/admin/g' wp-config.php
+      sed -i 's/password_here/password/g' wp-config.php
+      sed -i 's/localhost/${aws_db_instance.my_wordpress_db.address}/' wp-config.php
       chmod -R 755 wp-content
       chown -R apache:apache wp-content
       systemctl enable httpd && systemctl start httpd 
